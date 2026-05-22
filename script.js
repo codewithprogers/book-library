41d8ce17a3f75d622a045905b037bdc2d966488f
Patrick Rogers <225264952+codewithprogers@users.noreply.github.com>
2026-05-22 12:43:45 -0700

    feat: add book constructor & library storage logic
    Create a Book constructor with title, author, pages, read status, and a
    unique ID. Add logic to create new book objects, store them in the
    myLibrary array, and return book details with the info prototype method.

---
+++ script.js
@@ -0,0 +1,24 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+  if (!new.target) {
+    throw Error("You must use the 'new' operator to call the constructor");
+  }
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.ID = crypto.randomUUID();
+}
+
+function addBookToLibrary(title, author, pages, read) {
+  const newBook = new Book(title, author, pages, read);
+  myLibrary.push(newBook);
+}
+
+Book.prototype.info = function () {
+  return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}.`;
+};
+
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, "not read yet");
+console.log(myLibrary[0].info());
