2c8291ce0734e28578f7bf444573319682a1cf53
Patrick Rogers <225264952+codewithprogers@users.noreply.github.com>
2026-05-27 19:48:17 -0700

    feat: Create function that displays books
    Write a function that loops through the myLibrary array and displays
    each book on the page. Create a div element with the class of book-card
    that will contain the books info. Prepare for styling the layout of the
    book card.

--- script.js
+++ script.js
@@ -21,4 +21,34 @@ Book.prototype.info = function () {
 };
 
 addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, "not read yet");
-console.log(myLibrary[0].info());
+addBookToLibrary("Good to Great", "Jim Collins", 320, "read");
+addBookToLibrary("How to Win Friends and Influence People", "Dale Carnegie", 320, "read");
+
+const list = document.querySelector(".book-list");
+function displayBooks() {
+  for (const book of myLibrary) {
+    const card = document.createElement("div");
+    card.classList.add("book-card");
+
+    const title = document.createElement("h3");
+    title.textContent = book.title;
+
+    const author = document.createElement("p");
+    author.textContent = book.author;
+
+    const pages = document.createElement("p");
+    pages.textContent = `${book.pages} pages`;
+
+    const readStatus = document.createElement("p");
+    readStatus.textContent = book.read;
+
+    card.appendChild(title);
+    card.appendChild(author);
+    card.appendChild(pages);
+    card.appendChild(readStatus);
+
+    list.appendChild(card);
+  }
+}
+
+displayBooks();
